5ac8f82bc59c260d520bdda4c637069bc25a930b
Eric Honingh <erichoningh@codedazur.com>
2022-06-28 19:53:06 +0200

    hosturl server changed

--- gapp-server/app.js
+++ gapp-server/app.js
@@ -20,7 +20,7 @@ const { Server } = require('socket.io');
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000' || 'https://iron-gapp.herokuapp.com/',
+    origin: 'http://localhost:3000' || 'http://iron-gapp.netlify.com',
     methods: ['GET', 'POST']
   }
 });
