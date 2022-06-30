6c6b1531518f2504b2ddcb907b0af269faf6d154
Roku013 <roku.mail@gmail.com>
2022-06-30 19:34:06 +0200

    changed to false

--- server/express-session-options.js
+++ server/express-session-options.js
@@ -8,7 +8,7 @@ const expressSessionOptions = {
   proxy: true,
   cookie: {
     maxAge: 15 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
+    httpOnly: false,
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : false,
     secure: process.env.NODE_ENV === 'production'
   },
