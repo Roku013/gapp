203dde15942da138491bcd70af9ed878f1f55dc8
Arundathi Jayarathne <sandyjayarathne@gmail.com>
2022-07-02 10:56:19 +0200

    removed imagekit

--- server/routes/base.js
+++ server/routes/base.js
@@ -2,7 +2,6 @@
 
 const express = require('express');
 const router = express.Router();
-const ImageKit = require('imagekit');
 const routeGuard = require('./../middleware/route-guard');
 
 router.get('/', (req, res, next) => {
@@ -11,18 +10,6 @@ router.get('/', (req, res, next) => {
 
 router.get('/private', routeGuard, (req, res, next) => {
   res.json({});
-});
-
-router.get('/imagekit-authentication', (req, res, next) => {
-  const imagekit = new ImageKit({
-    publicKey: process.env.IMAGEKIT_PUBLIC_API_KEY,
-    privateKey: process.env.IMAGEKIT_PRIVATE_API_KEY,
-    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
-  });
-
-  const authenticationParameters = imagekit.getAuthenticationParameters();
-
-  res.json(authenticationParameters);
 });
 
 module.exports = router;
