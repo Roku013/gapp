a7d355d9b3ae94c9fa308104320dc105681654ce
Roku013 <roku.mail@gmail.com>
2022-07-02 11:16:48 +0200

    removed params

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import AuthenticationContext from '../context/authentication';
 import { registerUser } from '../services/authentication';
 import { Link } from 'react-router-dom';
