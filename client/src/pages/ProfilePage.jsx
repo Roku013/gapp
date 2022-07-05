09ae5bc788a9dec1ef9b3658e7ccf1273cbb1723
Eric Honingh <erichoningh@codedazur.com>
2022-07-05 17:29:29 +0200

    created navigation only on profile yet

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -59,6 +59,27 @@ const ProfilePage = () => {
           </p>
         </div>
       )}
+      <div className="navigation-bottom">
+        <div className="circle">
+          <Link to="/group">
+            <img
+              className="groups-icon"
+              src="/images/groups.svg"
+              alt="Groups"
+            />
+          </Link>
+          <Link to="/group">
+            <img className="events-icon" src="/images/event.svg" alt="Events" />
+          </Link>
+          <Link className="active" to="/group">
+            <img
+              className="profile-icon"
+              src="/images/profile.svg"
+              alt="Profile"
+            />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
