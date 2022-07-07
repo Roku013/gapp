2def62197e08f2480d23ff41621d77fed9800000
Eric Honingh <erichoningh@codedazur.com>
2022-07-07 20:33:12 +0200

    updated home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,27 +1,8 @@
 // import { useContext } from 'react';
-import { Link } from 'react-router-dom';
 import './Navbar.scss';
 
 const Navbar = () => {
-  return (
-    <div className="navigation-bottom">
-      <div className="circle">
-        <Link to="/group">
-          <img className="groups-icon" src="/images/groups.svg" alt="Groups" />
-        </Link>
-        <Link to="/group">
-          <img className="events-icon" src="/images/event.svg" alt="Events" />
-        </Link>
-        <Link className="active" to="/group">
-          <img
-            className="profile-icon"
-            src="/images/profile.svg"
-            alt="Profile"
-          />
-        </Link>
-      </div>
-    </div>
-  );
+  return <div className="navigation-bottom"></div>;
 };
 
 export default Navbar;
